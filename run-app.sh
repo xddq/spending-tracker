00eac99becad528ad9e31944acd10dfc6f2fdac1
Pierre Dahmani <hi@pierre-dev.com>
2023-07-27 23:15:32 +0200

    adds dmate into run-app. adds dmate 2.5.0 into dockerfile

--- run-app.sh
+++ run-app.sh
@@ -2,5 +2,10 @@
 #
 # only used for docker deployment, executed after wait-for-it.sh
 
+# creates database (required on first ever run, idempotent)
+dbmate create
+# runs migrations
+dbmate up
+
 echo "$(date) - Starting app"
 cabal run todo-app
